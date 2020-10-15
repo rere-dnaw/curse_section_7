d4792aa235b040cdb019fedca4b61c557bf46e67
rere-dnaw-PC <rafal.wilk92@gmail.com>
2020-10-15 16:40:15 +0100

    end of section 7

--- tests/base_test.py
+++ tests/base_test.py
@@ -14,8 +14,18 @@ from db import db
 class BaseTest(TestCase):
     @classmethod
     def setUpClass(cls):
+        """
+        If error "AssertionError: A setup function was called after the first request was handled."
+        occurs, this is because the app is passed to SClAlchemy db.init(app). This happened when the app
+        debugging mode is set to TRUE(in debugging mode flask is checking much more things).
+        To make it go away the debugging needs to be set to false and PROPAGATE_EXCEPTIONS needs to be
+        set to TRUE.
+        """
+        app.config['PROPAGATE_EXCEPTIONS'] = True
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
         with app.app_context():
+
             db.init_app(app)
             db.create_all()
 
